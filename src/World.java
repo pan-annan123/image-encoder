5685e5fb5daf22fe228e6092f4306746b991e600
genesis.123 <pan.annan123@gmail.com>
2016-10-30 21:52:23 -0400

    fixed bugs

--- src/World.java
+++ src/World.java
@@ -17,6 +17,7 @@ public class World extends JPanel {
 	private BufferedImage canvas;
 
 	private static String root = "";
+	private static int prefix = 8, buffer = 4;
 
 	public World() {
 		File file = new File(root+"input");
@@ -24,8 +25,8 @@ public class World extends JPanel {
 			System.out.println("MUST HAVE VALID ROOT AND INPUT FILE");
 			return;
 		}
-		long l = file.length()+4;
-		long p = l % 4 == 0? l/4 : l/4+1;
+		long l = file.length()+prefix;
+		long p = l % buffer == 0? l/buffer : l/buffer+1;
 		int w = (int) Math.sqrt(p) +1;
 
 		canvas = new BufferedImage(w, w, BufferedImage.TYPE_INT_ARGB);
@@ -64,31 +65,35 @@ public class World extends JPanel {
 
 		FileOutputStream fos = new FileOutputStream(root+"generated");
 
-		int zeros = 0;
-		for (int x=pixels.length-1; x>0; x--) {
-			if (pixels[x] == 0) {
-				zeros ++;
-			} else {
-				break;
-			}
-		}
-
-		final int pixelLength = 4;
+		final int pixelLength = buffer;
 
 		int len = 0;
 		len += (((int) pixels[0] & 0xff) << 24); // alpha
 		len += ((int) pixels[1] & 0xff); // blue
 		len += (((int) pixels[2] & 0xff) << 8); // green
 		len += (((int) pixels[3] & 0xff) << 16); // red
-		//        System.out.println(argb);
-		for (int pixel = 4; pixel < pixels.length - zeros; pixel += pixelLength) {
-			byte[] buffer = new byte[4];
+		int len2 = 0;
+		len2 += (((int) pixels[4] & 0xff) << 24); // alpha
+		len2 += ((int) pixels[5] & 0xff); // blue
+		len2 += (((int) pixels[6] & 0xff) << 8); // green
+		len2 += (((int) pixels[7] & 0xff) << 16); // red
+		System.out.println(len);
+		System.out.println(len2);
+		System.out.println("===");
+
+		long length = (long) len << 32 | len2 & 0xFFFFFFFFL;
+		int mod = (int) (length % buffer);
+		System.out.println("TOTAL PIXELS "+pixels.length);
+		System.out.println("LENGTH "+length);
+		System.out.println("MOD "+mod);
+		for (int pixel = prefix; pixel < prefix+length; pixel += pixelLength) {
+			byte[] buffer = new byte[pixelLength];
 			buffer[0] = (byte) (pixels[pixel] & 0xff); // alpha
 			buffer[3] = (byte) (pixels[pixel + 1] & 0xff); // blue
 			buffer[2] = (byte) (pixels[pixel + 2] & 0xff); // green
 			buffer[1] = (byte) (pixels[pixel + 3] & 0xff); // red
-			if (pixel + 4 >= pixels.length - zeros) {
-				fos.write(buffer, 0, len);
+			if (pixel + pixelLength >= prefix+length) {
+				fos.write(buffer, 0, mod);
 			} else {
 				fos.write(buffer);
 			}
@@ -101,17 +106,25 @@ public class World extends JPanel {
 	public void read(File file, Consumer consumer) {
 		FileInputStream fileInputStream = null;
 
-		byte[] buffer = new byte[4];
+		byte[] buffer = new byte[World.buffer];
 
 		try {
 			fileInputStream = new FileInputStream(file);
-			int trailing = (int)((file.length()%4));
-			consumer.write(trailing);
+
+			long ab = file.length();
+			System.out.println("LENGTH "+ab);
+			int c = (int) (ab >> 32);
+			int d = (int) ab;
+			System.out.println(c);
+			System.out.println(d);
+			System.out.println("===");
+			consumer.write(c);
+			consumer.write(d);
 			while ((fileInputStream.read(buffer)) > 0) {
-				int hex = (buffer[0] << 24) | (buffer[1] << 16) | (buffer[2] << 8) | buffer[3];
-				//				System.out.println(Integer.toHexString(hex));
+				int hex = (0xff & buffer[0]) << 24 | (0xff & buffer[1]) << 16 | (0xff & buffer[2]) << 8 | (0xff & buffer[3]);
+//				 System.out.println(String.format("[0]: %1$d, [1]: %2$d, [2]: %3$d, [3]: %4$d", buffer[0],buffer[1], buffer[2], buffer[3]));
 				consumer.write(hex);
-				buffer = new byte[4];
+				buffer = new byte[World.buffer];
 			}
 			fileInputStream.close();
 		}catch(Exception e){
